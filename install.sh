77ed159c52fdb0906b0855ada4e8d80268930f93
Naoyuki Kawano <naoyuki.kawano@nhk-mt.co.jp>
2016-03-22 12:30:32 +0900

    mod

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+cd ~
 mkdir -p ~/.vim/bundle
 mkdir  ~/.vim/undo
 mkdir  ~/.vim/colors
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-git clone https://github.com/tomasr/molokai.git
-ln -s ~/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
+git clone https://github.com/tomasr/molokai.git ~/.vim/molokai
+ln -s ~/.vim/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
 ./dotfilesLink.sh
