78aece6a24b758b6d4f3585ed26b31237409ccf4
Naoyuki Kawano <naoyuki.kawano@nhk-mt.co.jp>
2016-03-25 20:34:50 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ mkdir  ~/.vim/colors
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 git clone https://github.com/tomasr/molokai.git ~/.vim/molokai
 ln -s ~/.vim/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
-./dotfilesLink.sh
+./dotfiles/dotfilesLink.sh
